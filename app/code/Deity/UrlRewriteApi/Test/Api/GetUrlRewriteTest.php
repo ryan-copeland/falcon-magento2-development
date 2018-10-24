f59a9b962742c8b3f8348f5d4568e18d4cf09fa9
Sergii Gozhedrianov <sergii@deity.io>
2018-10-24 12:45:26 +0200

    added web api tests

---
+++ app/code/Deity/UrlRewriteApi/Test/Api/GetUrlRewriteTest.php
@@ -0,0 +1,76 @@
+<?php
+declare(strict_types=1);
+
+namespace Deity\UrlRewriteApi\Test\Api;
+
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\TestFramework\TestCase\WebapiAbstract;
+
+class GetUrlRewriteTest extends WebapiAbstract
+{
+    /**
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    protected $objectManager;
+
+    /**
+     * @var \Magento\Framework\Filesystem
+     */
+    protected $fileSystem;
+
+    /**#@+
+     * Service constants
+     */
+    const RESOURCE_PATH = '/V1/url';
+
+    /**
+     * @param $existingUrl
+     * @return array|bool|float|int|string
+     */
+    public function getUrlRewriteInfo($existingUrl)
+    {
+        $serviceInfo = [
+            'rest' => [
+                'resourcePath' => self::RESOURCE_PATH . "?url=" . $existingUrl,
+                'httpMethod' => \Magento\Framework\Webapi\Rest\Request::HTTP_METHOD_GET,
+            ]
+        ];
+        $item = $this->_webApiCall($serviceInfo, []);
+        return $item;
+    }
+
+
+    protected function setUp()
+    {
+        $this->objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
+        $this->fileSystem = $this->objectManager->get(\Magento\Framework\Filesystem::class);
+    }
+
+
+    /**
+     * @magentoApiDataFixture ../../../../app/code/Deity/UrlRewriteApi/Test/_files/url_rewrite.php
+     */
+    public function testExecute()
+    {
+        $item = $this->getUrlRewriteInfo('page-a');
+        $this->assertEquals('CMS_PAGE', $item['entity_type'], "Item was retrieved successfully");
+
+        $item = $this->getUrlRewriteInfo('product-one');
+        $this->assertEquals('PRODUCT', $item['entity_type'], "Item was retrieved successfully");
+        
+        $item = $this->getUrlRewriteInfo('category-one');
+        $this->assertEquals('CATEGORY', $item['entity_type'], "Item was retrieved successfully");
+    }
+
+    protected function tearDown()
+    {
+        $rollbackfixturePath = $this->fileSystem->getDirectoryRead(DirectoryList::ROOT)
+                ->getAbsolutePath('/app/code/Deity/UrlRewriteApi/Test/_files/url_rewrite_rollback.php');
+        echo $rollbackfixturePath;
+            if (file_exists($rollbackfixturePath)) {
+                echo "RUN!";
+                include $rollbackfixturePath;
+            }
+    }
+
+}
