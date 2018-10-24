5cf23add0043b49608a4a782c80d214b38282b13
Sergii Gozhedrianov <sergii@deity.io>
2018-10-24 13:00:25 +0200

    re-added url rewrite module

---
+++ app/code/Deity/UrlRewriteApi/Api/GetUrlRewriteInterface.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Deity\UrlRewriteApi\Api;
+
+use Deity\UrlRewriteApi\Api\Data\UrlRewriteInterface;
+
+/**
+ * @package Deity\UrlRewriteApi\Api
+ */
+interface GetUrlRewriteInterface
+{
+    /**
+     * @param $url
+     * @return \Deity\UrlRewriteApi\Api\Data\UrlRewriteInterface
+     */
+    public function execute(string $url): UrlRewriteInterface;
+
+}
