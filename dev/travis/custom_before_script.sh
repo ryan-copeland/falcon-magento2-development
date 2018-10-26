bfbf273df5e8564303014079b0af5cb29e9f18d8
Sergii Gozhedrianov <sergii@deity.io>
2018-10-26 12:49:44 +0200

    introduced custom before test script

--- dev/travis/custom_before_script.sh
+++ dev/travis/custom_before_script.sh
@@ -11,54 +11,24 @@ case $TEST_SUITE in
     integration)
         cd dev/tests/integration
 
-        test_set_list=$(find testsuite/* -maxdepth 1 -mindepth 1 -type d | sort)
-        test_set_count=$(printf "$test_set_list" | wc -l)
-        test_set_size[1]=$(printf "%.0f" $(echo "$test_set_count*0.12" | bc))  #12%
-        test_set_size[2]=$(printf "%.0f" $(echo "$test_set_count*0.32" | bc))  #32%
-        test_set_size[3]=$((test_set_count-test_set_size[1]-test_set_size[2])) #56%
-        echo "Total = ${test_set_count}; Batch #1 = ${test_set_size[1]}; Batch #2 = ${test_set_size[2]}; Batch #3 = ${test_set_size[3]};";
-
-        echo "==> preparing integration testsuite on index $INTEGRATION_INDEX with set size of ${test_set_size[$INTEGRATION_INDEX]}"
-        cp phpunit.xml.dist phpunit.xml
-
-        # remove memory usage tests if from any set other than the first
-        if [[ $INTEGRATION_INDEX > 1 ]]; then
-            echo "  - removing testsuite/Magento/MemoryUsageTest.php"
-            perl -pi -0e 's#^\s+<!-- Memory(.*?)</testsuite>\n##ims' phpunit.xml
-        fi
-
-        # divide test sets up by indexed testsuites
-        i=0; j=1; dirIndex=1; testIndex=1;
-        for test_set in $test_set_list; do
-            test_xml[j]+="            <directory suffix=\"Test.php\">$test_set</directory>\n"
-
-            if [[ $j -eq $INTEGRATION_INDEX ]]; then
-                echo "$dirIndex: Batch #$j($testIndex of ${test_set_size[$j]}): + including $test_set"
-            else
-                echo "$dirIndex: Batch #$j($testIndex of ${test_set_size[$j]}): + excluding $test_set"
-            fi
-
-            testIndex=$((testIndex+1))
-            dirIndex=$((dirIndex+1))
-            i=$((i+1))
-            if [ $i -eq ${test_set_size[$j]} ] && [ $j -lt $INTEGRATION_SETS ]; then
-                j=$((j+1))
-                i=0
-                testIndex=1
-            fi
-        done
-
-        # replace test sets for current index into testsuite
-        perl -pi -e "s#\s+<directory.*>testsuite</directory>#${test_xml[INTEGRATION_INDEX]}#g" phpunit.xml
-
-        echo "==> testsuite preparation complete"
-
         # create database and move db config into place
         mysql -uroot -e '
             SET @@global.sql_mode = NO_ENGINE_SUBSTITUTION;
             CREATE DATABASE magento_integration_tests;
         '
         mv etc/install-config-mysql.travis.php.dist etc/install-config-mysql.php
+
+        cd ../../..
+        ;;
+    api-functional)
+        cd dev/tests/api-functional
+
+        # create database and move db config into place
+        mysql -uroot -e '
+            SET @@global.sql_mode = NO_ENGINE_SUBSTITUTION;
+            CREATE DATABASE magento_functional_tests;
+        '
+        mv config/install-config-mysql.travis.php.dist config/install-config-mysql.php
 
         cd ../../..
         ;;
