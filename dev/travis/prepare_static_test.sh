638fe6c1cce850017d6e8f317e6a629420e27ca1
Sergii Gozhedrianov <sergii@deity.io>
2018-10-26 10:20:35 +0200

    adjusting static test configuration

---
+++ dev/travis/prepare_static_test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cp dev/travis/config/static_php_common.txt dev/tests/static/testsuite/Magento/Test/Php/_files/whitelist/common.txt
+cp dev/tests/static/phpunit.xml.custom dev/tests/static/phpunit.xml
