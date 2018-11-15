7d217b3035c7520d60b1ac3e025079a15c8891c6
Sergii Gozhedrianov <sergii@deity.io>
2018-11-15 15:42:40 +0100

    refactor converted basic field added request_path

--- app/code/Deity/Catalog/Model/CategoryProductList.php
+++ app/code/Deity/Catalog/Model/CategoryProductList.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 namespace Deity\Catalog\Model;
 
 use Deity\CatalogApi\Api\CategoryProductListInterface;
-use Deity\CatalogApi\Api\Data\ProductInterfaceFactory;
+use Deity\CatalogApi\Api\ProductConvertInterface;
 use Deity\CatalogApi\Api\Data\ProductSearchResultsInterface;
 use Deity\CatalogApi\Api\Data\ProductSearchResultsInterfaceFactory;
+use Magento\Catalog\Api\CategoryRepositoryInterface;
+use Magento\Catalog\Model\Layer\Resolver;
+use Magento\Catalog\Model\Product;
+use Magento\Framework\Registry;
 
 /**
  * Class CategoryProductList
@@ -20,20 +24,50 @@ class CategoryProductList implements CategoryProductListInterface
      */
     private $productSearchResultFactory;
 
+
     /**
-     * @var ProductInterfaceFactory
+     * @var ProductConvertInterface
      */
-    private $productFactory;
+    private $productConverter;
+
+    /**
+     * Catalog layer
+     *
+     * @var \Magento\Catalog\Model\Layer
+     */
+    private $catalogLayer;
+
+    /**
+     * @var CategoryRepositoryInterface
+     */
+    private $categoryRepository;
+
+    /**
+     * @var Registry
+     */
+    private $registry;
+
 
     /**
      * CategoryProductList constructor.
      * @param ProductSearchResultsInterfaceFactory $productSearchResultFactory
-     * @param ProductInterfaceFactory $productFactory
+     * @param ProductConvertInterface $convert
+     * @param CategoryRepositoryInterface $categoryRepository
+     * @param Registry $registry
+     * @param Resolver $layerResolver
      */
-    public function __construct(ProductSearchResultsInterfaceFactory $productSearchResultFactory, ProductInterfaceFactory $productFactory)
-    {
+    public function __construct(
+        ProductSearchResultsInterfaceFactory $productSearchResultFactory,
+        ProductConvertInterface $convert,
+        CategoryRepositoryInterface $categoryRepository,
+        Registry $registry,
+        Resolver $layerResolver
+    ) {
+        $this->productConverter = $convert;
         $this->productSearchResultFactory = $productSearchResultFactory;
-        $this->productFactory = $productFactory;
+        $this->catalogLayer = $layerResolver->get();
+        $this->categoryRepository = $categoryRepository;
+        $this->registry = $registry;
     }
 
 
@@ -41,16 +75,27 @@ class CategoryProductList implements CategoryProductListInterface
      * @param int $categoryId
      * @param \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria
      * @return \Deity\CatalogApi\Api\Data\ProductSearchResultsInterface
+     * @throws \Magento\Framework\Exception\LocalizedException
      */
     public function getList(
         int $categoryId,
         \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria = null
     ): ProductSearchResultsInterface {
 
+        $currentCategory = $this->categoryRepository->get($categoryId);
+        $this->registry->register('current_category', $currentCategory);
+        $this->catalogLayer->setCurrentCategory($currentCategory);
+
+        $responseProducts = [];
+        foreach ($this->catalogLayer->getProductCollection() as $productObject) {
+            /** @var $productObject Product */
+            $responseProducts[] = $this->productConverter->convert($productObject);
+        }
         /** @var ProductSearchResultsInterface $productSearchResult */
         $productSearchResult = $this->productSearchResultFactory->create();
-        $product = $this->productFactory->create();
-        $productSearchResult->setItems([$product]);
+
+        $productSearchResult->setItems($responseProducts);
+
         return $productSearchResult;
     }
 }
