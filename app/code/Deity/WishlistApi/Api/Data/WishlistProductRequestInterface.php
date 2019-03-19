3be96c8af6147ef088f6b2de38018e8fb131f675
Ryan Copeland <ryan@ryancopeland.co.uk>
2019-03-19 17:11:52 +0000

    Remove setters from add product to wishlist request interface.

--- app/code/Deity/WishlistApi/Api/Data/WishlistProductRequestInterface.php
+++ app/code/Deity/WishlistApi/Api/Data/WishlistProductRequestInterface.php
@@ -10,21 +10,9 @@ interface WishlistProductRequestInterface
     public function getCustomerId(): int;
 
     /**
-     * @param mixed $customerId
-     * @return WishlistProductRequestInterface
-     */
-    public function setCustomerId($customerId);
-
-    /**
      * @return int
      */
     public function getProductId(): int;
-
-    /**
-     * @param int $productId
-     * @return WishlistProductRequestInterface
-     */
-    public function setProductId($productId);
 
     /**
      * @return int
@@ -32,27 +20,9 @@ interface WishlistProductRequestInterface
     public function getWishlistId(): int;
 
     /**
-     * @param int $wishlistId
-     * @return WishlistProductRequestInterface
-     */
-    public function setWishlistId($wishlistId);
-
-    /**
      * @return int
      */
     public function getQty(): int;
-
-    /**
-     * @param int $qty
-     * @return WishlistProductRequestInterface
-     */
-    public function setQty(int $qty);
-
-    /**
-     * @param array $superAttribute
-     * @return WishlistProductRequestInterface
-     */
-    public function setSuperAttribute(array $superAttribute);
 
     /**
      * @return mixed | null
