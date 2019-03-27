20a5c4ede551a6366afd789e91435d5fde824f23
Sergii Gozhedrianov <sergii@deity.io>
2019-03-27 13:09:45 +0100

    fixing setup for api functional tests

--- dev/travis/custom_before_script.sh
+++ dev/travis/custom_before_script.sh
@@ -5,16 +5,31 @@ trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit
 # prepare for test suite
 case $TEST_SUITE in
     api-functional)
+        echo "Installing Magento"
+        mysql -uroot -e 'CREATE DATABASE magento2;'
+        php bin/magento setup:install -q \
+            --language="en_US" \
+            --timezone="UTC" \
+            --currency="USD" \
+            --base-url="http://${MAGENTO_HOST_NAME}/" \
+            --admin-firstname="John" \
+            --admin-lastname="Doe" \
+            --backend-frontname="backend" \
+            --admin-email="admin@example.com" \
+            --admin-user="admin" \
+            --use-rewrites=1 \
+            --admin-use-security-key=0 \
+            --admin-password="123123q"
+
+        echo "Prepare api-functional tests for running"
         cd dev/tests/api-functional
 
-        # create database and move db config into place
-        mysql -uroot -e '
-            SET @@global.sql_mode = NO_ENGINE_SUBSTITUTION;
-            CREATE DATABASE magento_functional_tests;
-        '
-        sed -e "s?127.0.0.1:80?${MAGENTO_HOST_NAME}?g" --in-place ./phpunit.xml
+        sed -e "s?magento.url?${MAGENTO_HOST_NAME}?g" --in-place ./phpunit.xml
         mv config/install-config-mysql.travis.php.dist config/install-config-mysql.php
+
+        echo "Enabling production mode"
+        php bin/magento deploy:mode:set production
+
         echo "==> testsuite preparation complete"
-        cd ../../..
         ;;
 esac
