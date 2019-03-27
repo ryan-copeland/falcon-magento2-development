115958062d7c61036756868ecea168fd5751c065
Sergii Gozhedrianov <sergii@deity.io>
2019-03-27 13:55:45 +0100

    small fixes, adjusting db host configuration

--- dev/travis/custom_before_script.sh
+++ dev/travis/custom_before_script.sh
@@ -5,14 +5,15 @@ trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit
 # prepare for test suite
 case $TEST_SUITE in
     api-functional)
+        echo "Prepare api-functional tests for running"
+        cd dev/tests/api-functional
+
         echo "Installing Magento"
         # create database and move db config into place
         mysql -uroot -e '
             SET @@global.sql_mode = NO_ENGINE_SUBSTITUTION;
-            CREATE DATABASE magento_integration_tests;'
-
-        echo "Prepare api-functional tests for running"
-        cd dev/tests/api-functional
+            CREATE DATABASE magento_integration_tests;
+        '
 
         sed -e "s?magento.url?${MAGENTO_HOST_NAME}?g" --in-place ./phpunit.xml
         mv config/install-config-mysql.travis.php.dist config/install-config-mysql.php
